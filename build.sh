51313262e1a6f573781af6872ce13ee8c175ed2f
us0p <lopes.workstation@gmail.com>
2024-09-25 03:06:20 -0300

    fix: adjusting build file to enable rebuilding the project

--- build.sh
+++ build.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
+rm -rf ./venv
 python3 -m venv venv
 . ./venv/bin/activate
 pip install -r requirements.txt
 pyinstaller main.py -n reviewpln
-rm -rf ./build ./venv
 mkdir ~/.local/bin 2>/dev/null
+rm ~/.local/bin/reviewpln
 ln -s $(pwd)/dist/reviewpln/reviewpln ~/.local/bin/
 deactivate
